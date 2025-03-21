8eb63c1d100208b70109bcafed95e5048cd936a3
Niranjani1503 <niranjani0302@gmail.com>
2025-03-21 11:23:00 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,27 @@
+# Write your code here :-)
+from machine import Pin
+import time
+
+#importing time module
+#assinging a varible wanting the pin2 to be the output
+#Pin is a function and not a function
+
+#secb_led=Pin(2,Pin.OUT)
+
+#value 1 is for switching it on
+#value 0 is for switching it off
+
+"""
+secb_led.value(1)
+time.sleep(2)
+secb_led.value(0)
+time.sleep(2)
+"""
+
+"""
+while True:
+    secb_led.value(1)
+    time.sleep(2)
+    secb_led.value(0)
+    time.sleep(1)
+"""
